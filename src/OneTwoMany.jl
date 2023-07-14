6925ef5c0d034234cbbf766e53e47a2fc2baa002
Oliver Schulz <oschulz@mpp.mpg.de>
2023-07-14 15:00:17 +0200

    Fix Module docstring

--- src/OneTwoMany.jl
+++ src/OneTwoMany.jl
@@ -3,7 +3,8 @@
 """
     OneTwoMany
 
-Template for Julia packages.
+Provides provides utility functions for accessing the first, second and the
+rest of the elements of a collection.
 """
 module OneTwoMany
 
